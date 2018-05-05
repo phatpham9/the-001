9b2a979119f1518372458f86cee46ec2be185350
phatpham9 <phatpham9@gmail.com>
2018-05-06 00:54:05 +0700

    chore: add meta tags

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './Header.scss';
 
 const Header = props => (
-  <div className="header bg-light">
+  <div className="header" style={{background: 'white'}}>
     <div className="container">
       <nav class="navbar navbar-expand-lg navbar-light">
         <a class="navbar-brand" href="/" style={{color: '#42ab9e'}}>Celebrities.Star</a>
@@ -14,11 +14,11 @@ const Header = props => (
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="#">My Followings <span class="sr-only">(current)</span></a>
+              <a class="nav-link">My Followings <span class="sr-only">(current)</span></a>
             </li>
           </ul>
           <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search celeb" aria-label="Search celeb" />
+            <input class="form-control mr-sm-2" type="search" placeholder="Search celebrity..." aria-label="Search celebrity..." />
           </form>
         </div>
       </nav>
