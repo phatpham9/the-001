4afed25d1f44f80c2404d67543d7d776df8f5009
Khang Huynh <khanghuynh92@users.noreply.github.com>
2018-05-05 23:39:30 +0700

    feat(Close #5): Sort post list by newest posts (#6)
    * feat(Close #5): Sort post list by newest posts
    
    * feat(Add timestamp for post):

--- server/src/posts/controller.js
+++ server/src/posts/controller.js
@@ -2,7 +2,10 @@ const posts = require('../../posts.json');
 
 const list = async (req, res) => {
   const { celebId } = req.query;
-  const results = celebId ? posts.filter(post => post.celebId === celebId) : posts;
+  const sortPost = (a, b) => new Date(b.timestamp) - new Date(a.timestamp);
+  const results = celebId
+  ? posts.filter(post => post.celebId === celebId).sort(sortPost)
+  : posts.sort(sortPost);
 
   res.json({
     results,
