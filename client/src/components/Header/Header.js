09b0f69f1bd96d02cd358193e78dcce133f9519e
phatpham9 <phatpham9@gmail.com>
2018-05-06 15:12:27 +0700

    feat: handle celebs search box

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import './Header.scss';
 
+const FETCH_CELEBS_URL = '/api/celebs';
+
 const Header = props => (
-  <div className="header" style={{background: 'white'}}>
+  <div className="header">
     <div className="container">
       <nav class="navbar navbar-expand-lg navbar-light">
-        <a class="navbar-brand" href="/">
+        <Link class="navbar-brand" to="/">
           <img src="images/text-logo.svg" alt="Saostar" />
-        </a>
+        </Link>
+
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -22,13 +26,82 @@ const Header = props => (
               <a class="nav-link" href="javascript:void(0)">My Followings <span class="sr-only">(current)</span></a>
             </li>
           </ul>
-          <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search..." aria-label="Search..." />
-          </form>
+
+          <CelebsSearchBox />
         </div>
       </nav>
     </div>
   </div>
 );
+
+class CelebsSearchBox extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      celebs: [],
+      key: '',
+      results: [],
+    };
+
+    this.searchCelebs = this.searchCelebs.bind(this);
+    this.clearSearch = this.clearSearch.bind(this);
+  }
+
+  async getCelebs() {
+    try {
+      const response = await fetch(FETCH_CELEBS_URL);
+      const json = await response.json();
+
+      return json;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async componentDidMount() {
+    const { results } = await this.getCelebs();
+
+    this.setState({
+      celebs: results,
+    });
+  }
+
+  searchCelebs(key) {
+    this.setState({
+      key: key.trim(),
+    }, () => {
+      if (key.trim()) {
+        const results = this.state.celebs.filter(celeb => new RegExp(key.toLowerCase()).test(celeb.name.toLowerCase()));
+        console.log(key, results);
+
+        this.setState({
+          results,
+        });
+      }
+    });
+  }
+
+  clearSearch() {
+    this.setState({
+      key: '',
+      results: [],
+    });
+  }
+
+  render() {
+    return (
+      <form class="form-inline my-2 my-lg-0 dropdown">
+        <input class="form-control" type="search" placeholder="Search..." aria-label="Search..." value={this.state.key} onChange={e => this.searchCelebs(e.target.value)} />
+
+        {!!this.state.key && !!this.state.results.length && <div class="dropdown-menu show">
+          {this.state.results.map(celeb => (
+            <Link key={celeb.id} class="dropdown-item" to={`/celeb/${celeb.id}`}  onClick={this.clearSearch}>{celeb.name}</Link>
+          ))}
+        </div>}
+      </form>
+    );
+  }
+}
 
 export default Header;
