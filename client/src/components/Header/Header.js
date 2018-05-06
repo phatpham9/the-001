2ce0d442f2561b6aa626a5172437236f6bcb1d36
phatpham9 <phatpham9@gmail.com>
2018-05-06 14:34:01 +0700

    style: update logo & accent color

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -6,7 +6,9 @@ const Header = props => (
   <div className="header" style={{background: 'white'}}>
     <div className="container">
       <nav class="navbar navbar-expand-lg navbar-light">
-        <a class="navbar-brand" href="/" style={{color: '#42ab9e'}}>Celebrities.Star</a>
+        <a class="navbar-brand" href="/">
+          <img src="images/text-logo.svg" alt="Saostar" />
+        </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -14,11 +16,14 @@ const Header = props => (
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-              <a class="nav-link">My Followings <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="javascript:void(0)">Celebrities <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="javascript:void(0)">My Followings <span class="sr-only">(current)</span></a>
             </li>
           </ul>
           <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search celebrity..." aria-label="Search celebrity..." />
+            <input class="form-control mr-sm-2" type="search" placeholder="Search..." aria-label="Search..." />
           </form>
         </div>
       </nav>
