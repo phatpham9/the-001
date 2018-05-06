90cf93f8b6c3f7b3673638e83e6a720b92487368
phatpham9 <phatpham9@gmail.com>
2018-05-06 15:33:25 +0700

    feat: hide celeb page url in return

--- server/src/celebs/controller.js
+++ server/src/celebs/controller.js
@@ -2,8 +2,9 @@ const celebs = require('../../celebs.json');
 
 const list = async (req, res) => {
   res.json({
-    results: celebs,
+    results: celebs.map(({ id, name }) => ({ id, name })),
     count: celebs.length,
+    total: celebs.length,
   });
 };
 
